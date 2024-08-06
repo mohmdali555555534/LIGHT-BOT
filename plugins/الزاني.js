e45a4f35e80493d4127ce1e6a96ced4d16e586bf
mohmdali555555534 <lywnmalak@gmail.com>
2024-08-06 14:16:03 +0300

    Create الزاني.js

---
+++ plugins/الزاني.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+let user = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let k = Math.floor(Math.random() * 70);
+let top = `*${user(a)} 🐥انــت هــو الزاني والفتال عليك اللعنة ي راجل *`.trim()
+conn.sendFile (m.reply (top, null, { mentions: [a]}))}
+handler.help = handler.command = ['زاني']
+handler.tags = ['fun']
+handler.group = true
+handler.limit = 0
+export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
